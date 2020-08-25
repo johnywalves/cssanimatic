b0ee0d907d2c08625abc2cb0ff579431290260a0
Johny W. Alves <johnywalves@gmail.com>
2020-08-25 14:04:34 -0300

    :dizzy: Change actions JS to CSS

---
+++ README.md
@@ -0,0 +1,13 @@
+# CSS Animatic
+
+```bash
+yarn
+# or
+npm i
+```
+
+```bash
+yarn dev
+# or
+npm run dev
+```
--- script.js
+++ script.js
@@ -1,52 +1,48 @@
 var end = {
+  all: "scale(1.2)",
   transform: "rotate(90deg)",
   height: "30rem",
   width: "40rem",
   "box-shadow": "0.25rem 0.25rem 0.25rem 0.25rem var(--shadowBox)",
 };
 
-var start = {
-  transform: "",
-  height: "20rem",
-  width: "20rem",
-  "box-shadow": "1rem 1rem 1rem 0px var(--shadowBox)",
-};
-
-function mouseEnter() {
-  var property = document.querySelector("#form-transitions .property").value;
-  var box = document.getElementById("box");
-  if (property === "all") {
-    box.style.transform = "scale(1.2)";
-  } else {
-    box.style[property] = end[property];
-  }
-}
-
-function mouseLeave() {
-  var property = document.querySelector("#form-transitions .property").value;
-  var box = document.getElementById("box");
-  if (property === "all") {
-    box.style.transform = "";
-  } else {
-    box.style[property] = start[property];
-  }
-}
-
 function changeTransitions() {
+  var action = document.querySelector("#form-transitions #check-action")
+    .checked;
+
   var property = document.querySelector("#form-transitions .property").value;
   var delay = document.querySelector("#form-transitions .delay").value;
   var duration = document.querySelector("#form-transitions .duration").value;
   var func = document.querySelector("#form-transitions .function").value;
+  var box = document.getElementById("box");
 
-  var css =
-    property + " " + duration / 1000.0 + "s " + delay / 1000.0 + "s " + func;
+  var styleAnimations = document.getElementById("style-animations");
+  var styleTransitions = document.getElementById("style-transitions");
 
+  var delaySec = delay / 1000.0;
+  var durationSec = duration / 1000.0;
+
+  var prop = property !== "all" ? property : "transform";
+  if (action) {
+    styleAnimations.innerHTML =
+      "@keyframes animaframes { to { " + prop + ": " + end[property] + " } }";
+    styleTransitions.innerHTML = "";
+    box.style.animation =
+      "animaframes " + durationSec + "s " + delaySec + "s linear infinite";
+  } else {
+    styleAnimations.innerHTML = "";
+    styleTransitions.innerHTML =
+      "#box:hover { " + prop + ": " + end[property] + "; }";
+    box.style.animation = "";
+  }
+
+  var css = property + " " + durationSec + "s " + delaySec + "s " + func;
   var detailedHtml =
     "transition-delay: " +
-    delay / 1000.0 +
+    delaySec +
     "s;<br />" +
     "transition-duration: " +
-    duration / 1000.0 +
+    durationSec +
     "s;<br />" +
     "transition-property: " +
     property +
@@ -55,10 +51,12 @@ function changeTransitions() {
     func +
     ";";
 
-  document.getElementById("box").style.transition = css;
+  box.style.transition = css;
   document.querySelector(".painel-transitions .simple").innerHTML =
     "transition: " + css + ";";
   document.querySelector(
     ".painel-transitions .detailed"
   ).innerHTML = detailedHtml;
 }
+
+window.onload = changeTransitions;
